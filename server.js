5c1c4145216bae5cf6e7d2af3a776343853f3cff
Your Name <you@example.com>
2022-08-11 10:48:48 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const http = require("http");
+const port = 3000;
+
+const rotas = {
+  '/': 'Curso de Node',
+  '/livros': 'Entrou na pag de livros',
+  '/autores': 'Listagem de autores',
+  '/editora': 'Pag de editora',
+  '/sobre': 'Info sobre o projeto',
+
+}
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end(rotas[req.url]);
+});
+
+server.listen(port, () => {
+  console.log(`Servidor on em http://localhost:${port}`);
+});
